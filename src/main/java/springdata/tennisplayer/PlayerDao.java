e544118fbab7bcb241cfe8152ef128e2e3604028
Baisalbek Urustanbekov <chq-baisalbeku@corp.expeditors.com>
2022-05-09 16:27:39 -0700

    UPDATE method implementation

--- src/main/java/springdata/tennisplayer/PlayerDao.java
+++ src/main/java/springdata/tennisplayer/PlayerDao.java
@@ -50,4 +50,23 @@ public class PlayerDao {
                             player.getTitles()});
     }
 
+    /**
+     * UPDATE method
+     * @param player
+     * @return
+     */
+    public int updatePlayer(Player player){
+        String sql = "UPDATE PLAYER " +
+                "SET Name = ?, Nationality = ?, Birth_date = ? , Titles = ? " +
+                "WHERE ID = ?";
+
+        return jdbcTemplate.update( sql, new Object[] {
+                player.getName(),
+                player.getNationality(),
+                new Timestamp(player.getBirthDate().getTime()),
+                player.getTitles(),
+                player.getId() }
+        );
+    }
+
 }
--- src/main/java/springdata/tennisplayer/TennisPlayerApplication.java
+++ src/main/java/springdata/tennisplayer/TennisPlayerApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Date;
+import java.sql.Date;
+
 
 @SpringBootApplication
 public class TennisPlayerApplication implements CommandLineRunner {
@@ -32,6 +33,11 @@ public class TennisPlayerApplication implements CommandLineRunner {
 		logger.info("Inserting Player 4: {}", dao.insertPlayer(
 				new Player (4, "Thiem", "Austria",
 						new Date(System.currentTimeMillis()), 17 )));
+
+		//Updating a player
+		logger.info("Updating Player with Id 4: {}",  dao.updatePlayer(
+				new Player(4, "Thiem", "Austria",
+						Date.valueOf("1993-09-03"), 17)));
 
 		// Select all method
 		logger.info("All Players Data: {}", dao.getAllPlayers());
